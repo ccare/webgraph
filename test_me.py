0790f505cc8a0ea94a300c6e2f225e7f7d7d2558
Charles Care <c.p.care@gmail.com>
2010-12-03 09:04:43 +0000

    Simple test exampel

--- test_me.py
+++ test_me.py
@@ -1,3 +1,5 @@
+import unittest
+
 # Test as function
 def test_b():
     assert 'b' == 'b'
@@ -6,3 +8,8 @@ def test_b():
 class TestExampleTwo:
     def test_c(self):
         assert 'c' == 'c'
+
+# Old style unit test
+class ExampleTest(unittest.TestCase):
+    def test_a(self):
+        self.assert_(1 == 1)
